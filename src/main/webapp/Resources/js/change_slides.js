51a3ef5304b1d65e7167a0e2001ed9555e4572ea
Tomasz Bosak <bosak.tomasz+xubuntu14@gmail.com>
2015-05-28 21:52:17 +0200

    Solve issue 25
    Single content is disabled after 10 seconds and does not shown again
    see: https://github.com/iss-deweloper/iss/issues/25

--- src/main/webapp/Resources/js/change_slides.js
+++ src/main/webapp/Resources/js/change_slides.js
@@ -1,14 +1,16 @@
 $(function() {
 		
-			$("#slideshow > div:gt(0)").hide();
-	
-			setInterval(function() { 
-			  $('#slideshow > div:first')
+	if( $("#slideshow >div").size() > 1 )
+	{
+		$("#slideshow > div:gt(0)").hide();
+
+		setInterval(function() { 
+			$('#slideshow > div:first')
 			    .fadeOut(100)
 			    .next()
 			    .fadeIn(100)
 			    .end()
 			    .appendTo('#slideshow');
 			},  10000);
-			
-		});
+	}	
+});
